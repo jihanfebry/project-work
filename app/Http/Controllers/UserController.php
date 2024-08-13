56d64c5310bd674716b094b5e563021e8908a2e8
Zraquera21 <jihanfebry2102@gmail.com>
2024-08-13 09:04:21 +0700

    Update CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-
 use App\Models\User;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
