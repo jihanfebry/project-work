06a416f71f5d3e67dccc6275cc52861007d1d134
giblar <giblarzahranmaula0141@gmail.com>
2024-11-02 20:57:12 +0700

    halo

--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,7 @@ use App\Http\Controllers\LoginAuthController;
 
         Route::post('/users', [UserController::class, 'store']);
         Route::get('/users/{id}', [UserController::class, 'show']);
-        Route::put('/users/{id}', [UserController::class, 'update']);
+        Route::patch('/users/{id}', [UserController::class, 'update']);
         Route::delete('/users/{id}', [UserController::class, 'destroy']);
     
         Route::apiResource('/payment', PaymentController::class);
