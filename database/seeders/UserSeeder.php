0537db72aba8cbaa32404b28f706889223aa9ead
murtidew <murtid299@gmail.com>
2024-11-18 14:01:55 +0700

    seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,16 +13,30 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        
+    
         User::create([
             'name'=> 'admin',
             'username' => 'Administrator',
-            'name' => 'admin',
             'email' => 'adminschool@gmail.com',
             'password' => Hash::make('admin'),
-            'role' => 'admin'
+            'role' => 'admin',
         ]);
-        
+
+
+        User::create([
+            'name' => 'tes_teacher',
+            'username' => 'teacher',
+            'email' => 'teacher@gmail.com',
+            'password' => Hash::make('teacher123'),
+            'role' => 'guru',
+        ]);
+
+        User::create([
+            'name' => 'tes_siswa',
+            'username' => 'siswa',
+            'email' => 'siswa@gmail.com',
+            'password' => Hash::make('siswa123'),
+            'role' => 'siswa',
+        ]);
     }
-    
 }
